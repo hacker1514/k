d878306ec98bfcf08de529f746fd14e5d15b36f9
NIRANJAN CREATIONS <hackerenvironment1514@gmail.com>
2026-04-16 17:59:00 +0530

    get.py

--- get.py
+++ get.py
@@ -1,8 +1,9 @@
 from variables import variables
+from exp_engine import safe_eval
 def get(code):
     if len(code)!=1 and code[0]=="<":
         code.pop(0)
         for i in code:
-            variables[i]=input("\033[32m")
+            variables[i]=safe_eval(input("\033[32m"),variables)
     else:
         print("\033[33m::\033[31mError\033[33m::\033[4;36msyntax\033[0m\033[35m error !")
